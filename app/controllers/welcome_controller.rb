830beed8c6708a5202ab2dc6cc5718bdcb74a2cf
Ben Fischer <ben.fischer.810@gmail.com>
2015-01-31 19:27:09 -0600

    add page for submitting charges

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
+  skip_before_action :verify_authenticity_token 
+
   def index
   end
 
   def submit
-    flash[:notice] = 'Successfully paid'
+    flash[:notice] = "Successfully charged #{params[:cc_name]} $#{params[:amount]}"
     redirect_to root_path
   end
 end
