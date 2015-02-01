00df90789bb32ec529c1558359e791201f112c05
Ben Fischer <ben.fischer.810@gmail.com>
2015-02-01 13:56:59 -0600

    update payment form to include more fields

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,21 @@
 module WelcomeHelper
+  def blank_card_selected
+    "selected" if params[:cc_type] == ""
+  end
+
+  def visa_card_selected
+    "selected" if params[:cc_type] == "visa"
+  end
+
+  def mastercard_card_selected
+    "selected" if params[:cc_type] == "mastercard"
+  end
+
+  def discover_card_selected
+    "selected" if params[:cc_type] == "discover"
+  end
+
+  def amex_card_selected
+    "selected" if params[:cc_type] == "amex"
+  end
 end
--- config/initializers/filter_cc_info.rb
+++ config/initializers/filter_cc_info.rb
@@ -1,2 +1,2 @@
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:cc_number, :cc_month, :cc_year, :cc_cvv]
+Rails.application.config.filter_parameters += [:cc_type, :cc_number, :cc_month, :cc_year, :cc_cvv]
