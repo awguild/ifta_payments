cc45911ae3acea0e2e901cb213a60b1a6ffde11a
Ben Fischer <ben.fischer.810@gmail.com>
2015-02-01 17:09:24 -0600

    override the cert setting the paypal gem

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,2 +1,7 @@
 PayPal::SDK.load("config/paypal.yml", Rails.env)
 PayPal::SDK.logger = Rails.logger
+module PayPal::SDK::Core::Util::HTTPHelper
+  def default_ca_file
+    File.expand_path(ENV['SSL_CERT_FILE'])
+  end
+end
